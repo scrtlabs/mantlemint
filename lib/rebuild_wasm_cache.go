62dede4e871e2042dd1807256bbeaa5bc6ce1d65
jess <jesse@soob.co>
2022-03-23 20:09:09 +0900

    fix(rebuild): hex decode

--- lib/rebuild_wasm_cache.go
+++ lib/rebuild_wasm_cache.go
@@ -1,11 +1,13 @@
 package lib
 
 import (
+	"encoding/hex"
 	wasm "github.com/CosmWasm/wasmvm/api"
 	wasmconfig "github.com/terra-money/core/x/wasm/config"
 	"github.com/terra-money/core/x/wasm/types"
 	"io/ioutil"
 	"log"
+	"path/filepath"
 )
 
 // RebuildWasmCache loads everything under dataDir and compile it by calling GetCode
@@ -14,7 +16,7 @@ func RebuildWasmCache(
 	supportedFeatures string,
 	cacheSize uint32,
 ) error {
-	blobs, err := ioutil.ReadDir(dataDir)
+	blobs, err := ioutil.ReadDir(filepath.Join(dataDir, "state/wasm"))
 
 	if err != nil {
 		return err
@@ -30,11 +32,16 @@ func RebuildWasmCache(
 
 	for _, blob := range blobs {
 		blobName := blob.Name()
-		log.Printf("recompiling wasm blob %s..\n", blobName)
-		_, err := wasm.AnalyzeCode(cache, []byte(blobName))
-
-		if err != nil {
+		blobBytes, decodeErr := hex.DecodeString(blobName)
+		if decodeErr != nil {
 			return err
+		}
+
+		log.Printf("recompiling wasm blob %s..\n", blobName)
+		_, analyzeErr := wasm.AnalyzeCode(cache, blobBytes)
+
+		if analyzeErr != nil {
+			return analyzeErr
 		}
 	}
 
